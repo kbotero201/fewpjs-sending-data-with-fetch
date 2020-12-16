9fd81aeb3dc4658c6c9eccf7435a3147b6c703ab
kbotero201 <kbotero201@gmail.com>
2020-12-16 16:32:41 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(name, email) {
+    return fetch( 'http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
+      })
+    
+    } )
+    // .then function 
+    .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        //console.log(data.id)
+        let body = document.querySelector("body")
+        body.textContent = data.id
+      })
+
+      // Error message .catch
+      .catch( function ( error ) {
+        let body = document.querySelector("body")
+        body.textContent = error.message
+      } )
+    
+}
